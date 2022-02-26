9e32a737b3c456c718896c06316828d0559bd844
William Barents <wbarents84@gmail.com>
2022-02-26 05:36:50 +0200

    add pagination buttons to the bottom of the list

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -1728,7 +1728,7 @@ parcelHelpers.export(exports, "RES_PER_PAGE", ()=>RES_PER_PAGE
 );
 const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 const TIMEOUT_SEC = 10;
-const RES_PER_PAGE = 10;
+const RES_PER_PAGE = 15;
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
 exports.interopDefault = function(a) {
